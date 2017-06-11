74b335207d84e0ce0f8c46d46e28326fbb2d537c
Abdelrahman Abdelghany <abdelrahmanabdelghany@Abdelrahmans-MacBook-Pro.local>
2017-06-11 12:05:27 +0200

    Initial Commit

---
+++ trialapp/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  trialapp
+//
+//  Created by Abdelrahman Abdelghany on 6/11/17.
+//  Copyright © 2017 Abdelrahman Abdelghany. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
